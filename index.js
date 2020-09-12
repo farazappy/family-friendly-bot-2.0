46c28f56903347f23f40ace84ba402e93ccb06b4
Faraz Ali <farazappy@esportsindia.in>
2020-09-12 11:32:39 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config({ path: __dirname + '/.env' })
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('pong');
+    }
+
+});
+
+client.login(process.env.AUTH_TOKEN);
