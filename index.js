396a46334b4cb40fbb447a735cdcc8db390abdc0
Faraz Ali <farazappy@esportsindia.in>
2020-09-12 12:24:22 +0530

    Meme

--- index.js
+++ index.js
@@ -8,9 +8,40 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
+    const rand = Math.floor((Math.random() * 100) + 1) == 100;
+
     if (msg.content === 'ping') {
         msg.reply('pong');
     }
+
+    if (msg.content.includes("password")) {
+        msg.reply("IT Cell will take it from here. Thank you.");
+    }
+
+    switch (msg.author.id) {
+        case '327365815785619457':
+            if (rand == 100) {
+                msg.reply("gift card please");
+            }
+            break;
+        case '252653111414358016':
+            if (rand == 100) {
+                msg.reply("It's Pawan, not Pavan.");
+            }
+            break;
+        case '240542597540610048':
+            if (rand == 100) {
+                msg.reply("Please fuck off.");
+            }
+            break;
+        default:
+            break;
+    }
+    // if (msg.author.id == "252653111414358016") {
+    //     if (Math.floor((Math.random() * 100) + 1) == 100) {
+    //         msg.reply("It's Pawan, not Pavan.");
+    //     }
+    // }
 
 });
 
