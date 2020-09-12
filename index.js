d9bf2fa7e7924bd37450892bfe0489c4f1c65847
Faraz Ali <farazappy@esportsindia.in>
2020-09-12 20:20:51 +0530

    Changes

--- index.js
+++ index.js
@@ -2,22 +2,68 @@ require('dotenv').config({ path: __dirname + '/.env' })
 
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const users = require('./retards.config')
+const axios = require('axios');
+const users = require('./retards.config');
+const ranks = require('./ranks.config');
+const openDotaKey = process.env.OPEN_DOTA_KEY;
+const axiosInstance = axios.create({
+    baseURL: 'https://api.opendota.com/api/',
+});
+
+function tierToRank (tier) {
+    const medal = tier.toString().charAt(0);
+    const star = tier.toString().charAt(1);
+
+    return `${ranks[medal]} ${star}`
+    // console.log(ranks[medal]);
+    // console.log(tier.toString().charAt(0));
+}
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-    const rand = Math.floor((Math.random() * 100) + 1) == 100;
+client.on('message', async msg => {
+    if (msg.content.charAt(0) != '>') {
+        return
+    }
 
-    if (msg.content === 'ping') {
+    const rand = Math.floor((Math.random() * 100) + 1) == 100;
+    const content = msg.content.substring(1, msg.content.length)
+
+    if (content === 'ping') {
         msg.reply('pong');
     }
 
-    if (msg.content.includes("password")) {
+    if (content.includes("password")) {
         msg.reply("IT Cell will take it from here. Thank you.");
     }
+
+    if (content === 'myrank') {
+        if (users.hasOwnProperty(msg.author.id)) {
+            await axiosInstance.get(`/players/${users[msg.author.id].steam32}`).then((r) => {
+                msg.reply(`Your Dota Rank is ${tierToRank(r.data.rank_tier)}, step up your game pleb!`);
+            }).catch((e) => {
+                console.log(e);
+            })
+        } else {
+            msg.reply('Premium membership required!');
+        }
+    }
+
+    if (content === 'mymmr') {
+        if (users.hasOwnProperty(msg.author.id)) {
+            await axiosInstance.get(`/players/${users[msg.author.id].steam32}`).then((r) => {
+                r.data.competitive_rank ? msg.reply(`Your Party MMR is ${r.data.competitive_rank}, eww!`) : (r.data.mmr_estimate ? msg.reply(`Your Party MMR is ${r.data.mmr_estimate.estimate}, eww!`) : msg.reply("Valve doesn't have your data, lmao noob!"));
+            }).catch((e) => {
+                msg.reply("That nearly killed me wew!");
+                console.log(e);
+            })
+        } else {
+            msg.reply('Premium membership required!');
+        }
+    }
+
 
     switch (msg.author.id) {
         case '327365815785619457': //Gagan
---
+++ ranks.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "1": "Herald",
+    "2": "Guardian",
+    "3": "Crusader",
+    "4": "Archon",
+    "5": "Legend",
+    "6": "Ancient",
+    "7": "Divine"
+}
