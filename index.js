c91f6f1de71d1c942b8cd80c38a7012586a3c861
aslicode <71381657+aslicode@users.noreply.github.com>
2020-09-16 18:45:16 +0530

    Update index.js
    how does the comparison op works in js wtf (Approve pls)

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const users = require('./retards.config');
 const ranks = require('./ranks.config');
 const openDotaKey = process.env.OPEN_DOTA_KEY;
 
-const admins = ['240542597540610048', '310860262624460801', '202035944260370441'];
+const admins = ['240542597540610048', '310860262624460801', '202035944260370441', '202744505114296331'];
 
 var delQueenTxt = false;
 
@@ -61,6 +61,16 @@ client.on('message', async msg => {
             msg.reply("Chalo theek hai!");
         }
     }
+    
+    if (content === 'modi') {
+        if (msg.author.id=240542597540610048 || msg.author.id=310860262624460801)) {
+            msg.reply("Kya bola be MULLE :dagger:")
+            return
+        } else {
+            delQueenTxt = false;
+            msg.reply("Hello Friend :pray_tone2: :) ");
+        }
+    }
 
     if (content === "mute") {
         if (!admins.includes(msg.author.id)) {
@@ -72,7 +82,7 @@ client.on('message', async msg => {
             msg.member.voice.channel.members.forEach((m) => {
                 if (m.roles.cache.some(role => role.name === 'Groovy'))
                     return;
-                m.voice.setMute(true, "Game Starating");
+                m.voice.setMute(true, "Game Starting");
             });
             // msg.member.guild.voiceStates.cache.forEach((c) => {
             //     if (msg.member.voice.channelID === c.channelID)
@@ -132,7 +142,6 @@ client.on('message', async msg => {
     }
 
     if (content.includes('asli')) {
-        msg.react(msg.guild.emojis.cache.find(emoji => emoji.name === 'pepehands'))
         msg.react(msg.guild.emojis.cache.find(emoji => emoji.name === 'sadsli'));
     }
 
