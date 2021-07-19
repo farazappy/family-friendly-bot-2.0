d80c87d0287dc8ab11872435309cc453a89b626c
red-vacuumpump <57187188+red-vacuumpump@users.noreply.github.com>
2021-07-19 19:52:13 +0530

    code by pavan

--- index.js
+++ index.js
@@ -205,7 +205,7 @@ client.on('message', async msg => {
     }
 
     if (content === "gagan") {
-        msg.reply("KHELNA SEEKH SARDAR!");
+        msg.reply("KHELNA SEEKH NA SARDAR!");
     }
 
     if (content === "whosimp") {
